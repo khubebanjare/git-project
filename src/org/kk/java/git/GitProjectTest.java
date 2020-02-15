56035dd0a33b643e39e3677ddc3592b97bdb1119
khube <khube@khube>
2020-02-15 17:07:53 +0530

    I had completed given task

--- src/org/kk/java/git/GitProjectTest.java
+++ src/org/kk/java/git/GitProjectTest.java
@@ -5,6 +5,11 @@ public class GitProjectTest {
 	public static void main(String[] args) {
 		System.out.println("This is your git project");
 		
-		//please add new behaviour and perform addition logic
+		//please add new behavior and perform addition logic
+		add(10, 20);
+	}
+	
+	public static int add(int a, int b) {
+		return a + b;
 	}
 }
